01a5ff0f9b418e6e1adbec9baf4e17a66fa05fa1
Rémy HUBSCHER <remy.hubscher@novapost.fr>
2013-10-18 14:51:55 +0200

    Improve the test_app

--- x509/test_app.py
+++ x509/test_app.py
@@ -1,31 +1,31 @@
-from __future__ import print_function
-import sys
-
-from flask import Flask, request
+from flask import Flask, request, make_response
 from cStringIO import StringIO
-from pprint import pprint
 
 application = Flask(__name__)
 
 
 @application.route('/')
 def home():
-    backup = sys.stdout
-    sys.stdout = StringIO()
+    response = StringIO()
 
     if 'HTTP_SSL_CLIENT_VERIFY' in request.environ:
-        print('HTTP_SSL_CLIENT_VERIFY: PRESENT\n<br/>')
+        response.write('HTTP_SSL_CLIENT_VERIFY: PRESENT\n\n')
 
         if request.environ['HTTP_SSL_CLIENT_VERIFY'] == 'SUCCESS':
             for var in ['HTTP_SSL_CLIENT_DN', 'HTTP_SSL_CLIENT_SERIAL']:
-                print('%s: %s' % (
-                    var,var in request.environ and 'PRESENT' or 'NOT PRESENT'))
-        print('\n\n')
+                response.write('%s: %s\n' % (
+                    var,
+                    var in request.environ and 'PRESENT' or 'NOT PRESENT'))
+            response.write('\n\n')
 
-    pprint(request.environ)
-    value = sys.stdout.getvalue()
-    sys.stdout = backup
-    return value
+    for key in request.environ.iterkeys():
+        if key.startswith('HTTP'):
+            response.write('%s: %s\n' % (key, request.environ[key]))
+
+    response = make_response(response.getvalue())
+    response.mimetype = 'text/plain'
+    response.status_code = 200
+    return response
 
 
 def main():
