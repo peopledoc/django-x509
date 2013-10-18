0a5c12253474657383f3b0e7f4e8864c5c74fed6
Rémy HUBSCHER <remy.hubscher@novapost.fr>
2013-10-18 16:16:25 +0200

    Add administration

---
+++ x509/django/admin.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+from x509.django.models import Certificate, GenericCertificateAdmin
+
+
+class CertificateAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Certificate, CertificateAdmin)
+
+
+class GenericCertificateM2MAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(GenericCertificateM2M, GenericCertificateM2MAdmin)
